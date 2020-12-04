a7e8a1e4bbd8904b9c051040a307f79eaa1af008
Andrew Kane <andrew@ankane.org>
2020-12-04 03:55:20 -0800

    Use direct link

--- index.js
+++ index.js
@@ -54,7 +54,8 @@ if (isMac()) {
 } else if (isWindows()) {
   let url;
   if (sqlserverVersion == 2019) {
-    url = 'https://go.microsoft.com/fwlink/?linkid=866662';
+    // https://go.microsoft.com/fwlink/?linkid=866662
+    url = 'https://download.microsoft.com/download/d/a/2/da259851-b941-459d-989c-54a18a5d44dd/SQL2019-SSEI-Dev.exe';
   } else {
     throw `SQL Server version not supported on Windows: ${sqlserverVersion}`;
   }
