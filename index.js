4cd456c96808bb8a8192cef529b29a1a073c80b0
Kirill Müller <krlmlr@mailbox.org>
2020-12-25 10:41:44 +0100

    Dogfood

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ if (isMac()) {
 } else {
   // install
   run(`wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -`);
+  // Use 18.04 repo in 20.04: https://computingforgeeks.com/how-to-install-ms-sql-on-ubuntu/
   run(`wget -qO- https://packages.microsoft.com/config/ubuntu/$(. /etc/os-release && echo $VERSION_ID | sed 's/20[.]04/18.04/')/mssql-server-${sqlserverVersion}.list | sudo tee /etc/apt/sources.list.d/mssql-server-${sqlserverVersion}.list`);
   // need to update all due to dependencies
   // run(`sudo apt-get update -o Dir::Etc::sourcelist="sources.list.d/mssql-server-${sqlserverVersion}.list" -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"`);
